59372ee613a5f1a37165034bad69f759c1f07186
Nola Chen <chen.no@husky.neu.edu>
2021-02-20 22:43:01 -0800

    init gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  siteMetadata: {
+    title: "Not The Pope's Van",
+  },
+  plugins: [
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-remark",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
